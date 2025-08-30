c92983ace1ef8de2a8374652a8e532953437593b
Tom Bulatewicz <tom@nosleep.net>
2025-08-29 20:56:03 -0700

    Make the test app launchable

---
+++ Ynab.Api.Client.Mock.TestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Ynab.Api.Client.Mock.TestApp;
+
+/// <summary>
+/// A program for testing the <see cref="YnabApiClientMock"/> class.
+/// </summary>
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        _ = new YnabApiClientMock();
+    }
+}
